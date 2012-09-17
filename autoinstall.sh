66dc9a3ca27c8feb2a0865afbfbae8843d4b4d30
geetotes <lee@fishburd.com>
2012-09-17 03:50:40 +0000

    working on autoinstaller

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,5 +1,23 @@
 #/bin/bash
 echo -p "Autoinstalling..."
 # TODO put a check for existing .bash_profile
-`rm -rf ~/.bash_profile`
-`ln -s profile ~/.bash_profile`
+
+# TODO
+# Should loop through existing files in directory
+# and link extensioned files into ther approrate place
+# and directories get linked as well
+
+workDir=`PWD`
+
+handleFile () {
+[ -f "~/$f" ] && unlink -rf "~/$f" 
+ln -s "$PWD/$f" "~/.$f"
+}
+
+for f in profile vimr screenrc
+do
+handleFile
+done
+
+
+
