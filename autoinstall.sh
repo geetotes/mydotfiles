e31c84f94c1cb31c50d11fabe2904ee4b39d804f
Lee Gillentine <lee@fishburd.com>
2012-09-17 00:20:53 -0400

    Finishing up autoinstall script

--- autoinstall.sh
+++ autoinstall.sh
@@ -11,13 +11,23 @@ echo "Autoinstalling..."
 handleFile () {
 [ -f "$HOME/.$f" ] && unlink "$HOME/.$f" 
 ln -s "$PWD/$f" "$HOME/.$f" 
-echo $PWD
 }
 
-for f in profile vimrc screenrc
+handleDir () {
+[ -d "$HOME/$d" ] && unlink "$HOME/$d" 
+ln -s "$PWD/$d" "$HOME/$d" 
+}
+
+#check for these files
+for f in bash_profile vimrc screenrc
 do
-handleFile
+  handleFile
 done
 
+#check for these directories
+for d in streams vim
+do
+  handleDir
+done
 
 
